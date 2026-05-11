96c66bbef3bc05367d1ac1ef21b78b5bb84f16ee
ceceli33 <ceceliccc33@gmail.com>
2026-05-11 16:41:02 +0300

    Create TITAN_v4.3_RUNNER.py

---
+++ TITAN_v4.3_RUNNER.py
@@ -0,0 +1,29 @@
+{
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {},
+      "outputs": [],
+      "source": [
+        "# 🔱 TITAN PHASE 4.3 | Master Runner (Cerrahi Müdahale)\n",
+        "# Mustafa Akbas - Mersin\n",
+        "!wget -q -O T43.py https://raw.githubusercontent.com/ceceli33/titan-cognitive-core/main/TITAN_PHASE_4.3_Final.py\n",
+        "%run T43.py"
+      ]
+    }
+  ],
+  "metadata": {
+    "kernelspec": {
+      "display_name": "Python 3",
+      "language": "python",
+      "name": "python3"
+    },
+    "language_info": {
+      "name": "python",
+      "version": "3.10"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
